ed64ccf07956f01a7b57ca77b9241d201a1dacd6
sumit9696-motto <sumit.rai9696@gmail.com>
2024-12-10 17:56:00 +0530

    binary to dec done

---
+++ binaqrytodecimal.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int binarytodec(int binarys){
+    int ans=0,pow=1;
+    while (binarys>0)
+
+    {
+       int rem=binarys%2;
+        ans+=(rem*pow);
+        binarys =binarys/10;
+        pow=pow*2;
+        
+
+    }
+    return ans;
+    
+}
+int main()
+{
+    int binarys  = 101110;
+   cout << binarytodec(binarys);
+    return 0;
+}
